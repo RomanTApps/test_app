bdb88ae1d0d86b0a7d1a39bba5173d12b9476ac8
romantsisyk <romantsisyk1@gmail.com>
2020-03-20 23:17:15 +0100

    test app ver 3.4.0

---
+++ app/src/main/java/romant/app/test/MainActivity.kt
@@ -0,0 +1,251 @@
+package romant.app.test
+
+import android.annotation.SuppressLint
+import android.graphics.Rect
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.View.*
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.elconfidencial.bubbleshowcase.BubbleShowCaseBuilder
+import com.elconfidencial.bubbleshowcase.BubbleShowCaseSequence
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.nightonke.boommenu.BoomButtons.HamButton
+import com.nightonke.boommenu.BoomMenuButton
+import com.nightonke.boommenu.ButtonEnum
+import kotlinx.android.synthetic.main.activity_content.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var bmb: BoomMenuButton? = null
+    private var mediaPlayer: MediaPlayer? = null
+    private var interstitialAd: InterstitialAd? = null
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private var SHOW_ID_1 = "SHOWI_ID1"
+    private var SHOW_ID_2 = "SHOWI_ID2"
+
+    private lateinit var runnable: Runnable
+    private var handler: Handler = Handler()
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bmbaction1()
+        MobileAds.initialize(this, getString(R.string.mobile_ad_id))
+        interstitialAd = newInterstitialAd()
+        loadInterstitial()
+        all()
+        var player_view = findViewById<View>(R.id.player_layout)
+        player_view.visibility = VISIBLE
+        mediaPlayer = MediaPlayer.create(this, R.raw.item1)
+
+        play.setOnClickListener {
+            when (player_view.visibility) {
+                VISIBLE -> player_view.visibility = GONE
+                GONE -> player_view.visibility = VISIBLE
+            }
+        }
+
+        initializeSeekBar()
+        btnPlay.setOnClickListener {
+            mediaPlayer?.start()
+        }
+        btnPause.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+        btnStop.setOnClickListener {
+            mediaPlayer?.pause()
+            mediaPlayer?.seekTo(0)
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
+            initializeSeekBar()
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if (p2) {
+                    mediaPlayer?.seekTo(p1 * 1000)
+                }
+            }
+        })
+    }
+
+    private fun showInterstitial() {
+        if (interstitialAd?.isLoaded == true) interstitialAd?.show() else {
+            interstitialAd = newInterstitialAd()
+            loadInterstitial()
+        }
+    }
+
+    private fun newInterstitialAd(): InterstitialAd {
+        return InterstitialAd(this).apply {
+            adUnitId = getString(R.string.interstitial_ad_id)
+            adListener = object : AdListener() {
+                override fun onAdLoaded() {}
+                override fun onAdFailedToLoad(errorCode: Int) {}
+                override fun onAdClosed() {
+                    Snackbar.make(findViewById(android.R.id.content),
+                            "Thank you for ads watching!", Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun loadInterstitial() {
+        val adRequest = AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template")
+                .build()
+        interstitialAd?.loadAd(adRequest)
+    }
+
+    private fun bmbaction1() {
+        bmb = findViewById(R.id.bmb)
+        assert(bmb != null)
+        bmb!!.buttonEnum = ButtonEnum.Ham
+        for (i in 0 until bmb!!.piecePlaceEnum.pieceNumber()) {
+            showInterstitial()
+            bmb!!.run {
+                addBuilder(HamButton.Builder()
+                        .imagePadding(Rect(10, 10, 10, 10))
+                        .normalImageRes(R.drawable.ic_logo1 + i)
+                        .normalTextRes(R.string.title1 + i)
+                        .subNormalTextRes(R.string.app_name_long)
+                        .listener {
+                            showInterstitial()
+                            showItem()
+                            mediaPlayer?.stop()
+                            when (i) {
+
+                                0 -> {
+                                    item1.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item1)
+                                }
+                                1 -> {
+                                    item2.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item2)
+                                }
+                                2 -> {
+                                    item3.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item3)
+                                }
+                                3 -> {
+                                    item4.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item4)
+                                }
+                                4 -> {
+                                    item5.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item5)
+                                }
+                                5 -> {
+                                    item6.visibility = VISIBLE
+                                    mediaPlayer = MediaPlayer.create(context, R.raw.item6)
+                                }
+                            }
+                        }
+                )
+            }
+        }
+    }
+
+    private fun bmbshow(): BubbleShowCaseBuilder {
+        Log.i("SHOW", "BMB")
+
+        return BubbleShowCaseBuilder(this)
+                .title(getString(R.string.bmbshow_title))
+                .description(getString(R.string.bmbshow_description))
+                .imageResourceId((R.drawable.ic_agenda))
+                .targetView(view_bmb) //View to point out
+                .showOnce(SHOW_ID_2)
+
+    }
+
+    private fun appshow(): BubbleShowCaseBuilder {
+        Log.i("SHOW", "BMB")
+        return BubbleShowCaseBuilder(this)
+                .title(getString(R.string.play_this_text))
+                .description(getString(R.string.app_name_long))
+                .imageResourceId((R.drawable.google_voice))
+                .targetView(player_layout) //View to point out
+                .showOnce(SHOW_ID_1)
+    }
+
+    private fun all() {
+        BubbleShowCaseSequence()
+                .addShowCase(bmbshow()) //First BubbleShowCase to show
+                .addShowCase(appshow()) // This one will be showed when firstShowCase is dismissed
+
+                .show()
+    }
+
+    private fun showItem() {
+        item1.visibility = GONE
+        item2.visibility = GONE
+        item3.visibility = GONE
+        item4.visibility = GONE
+        item5.visibility = GONE
+        item6.visibility = GONE
+    }
+
+
+    private fun initializeSeekBar() {
+        seekBar!!.max = mediaPlayer!!.seconds
+
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer!!.currentSeconds
+            tv_pass.text = "${mediaPlayer!!.currentSeconds} sec"
+            val diff = mediaPlayer!!.seconds - mediaPlayer!!.currentSeconds
+            tv_due.text = "$diff sec"
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    // Creating an extension property to get the media player time duration in seconds
+    private val MediaPlayer.seconds: Int
+        get() {
+            return this.duration / 1000
+        }
+
+    // Creating an extension property to get media player current position in seconds
+    private val MediaPlayer.currentSeconds: Int
+        get() {
+            return this.currentPosition / 1000
+        }
+
+
+    private fun releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer!!.release()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer!!.pause()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releaseMediaPlayer()
+    }
+
+}
